b027c8e20d135e9facf1b8c24a40c0efdab31779
jermgaming <atinder999@gmail.com>
2025-01-02 14:34:20 +0530

    countVowels solved

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -5,8 +5,24 @@
   Once you've implemented the logic, test your code by running
 */
 
+function isVowel(str){
+  if(str=='a'||str=='A'||str=='E'||str=='e'||str=='I'||str=='i'||str=='o'||str=='O'||str=='u'||str=='U'){
+    return true;
+  }else {
+    return false;
+  }
+
+  }
 function countVowels(str) {
     // Your code here
+    let ans=0;
+    for(let i=0;i<str.length;i++){
+      if(isVowel(str[i])==true){
+        ans++;
+      }
+    }
+
+    return ans;
 }
 
 module.exports = countVowels;
